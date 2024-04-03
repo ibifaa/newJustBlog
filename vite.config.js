f238277bb8cf908d477d5c5abc0825a1555af177
devibifaa <ib2link@yahoo.com>
2024-04-03 18:28:37 +0100

    update on relative path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
-  base: "https://vercel.com/ibifaas-projects/",
   plugins: [tailwindcss, autoprefixer],
 });

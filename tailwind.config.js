b97423160727f54fb3d166219197345acbf46b9c
devibifaa <ib2link@yahoo.com>
2024-04-03 02:50:08 +0100

    update on file path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./*.{html,js}", "./pages/*.{html,js}", "./pages/**/*.html"],
+  content: [
+    "./*.{html,js}",
+    "./pages/*.{html,js}",
+    "./pages/**/*.html",
+    "./pages/**/*.{html,js,ts,jsx,tsx}',",
+  ],
   theme: {
     extend: {},
   },

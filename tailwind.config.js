749797d3687aa713a9a67f33eddae2505bc9a16c
devibifaa <ib2link@yahoo.com>
2024-04-08 13:17:44 +0100

    updated tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./*.{html,js}",
-    "./pages/*.{html,js}",
-    "./pages/**/*.html",
-    "./pages/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ["./*.{html,js}", "./register.{html,js}"],
   theme: {
     extend: {},
   },
